b4c992013ff5542633ee5c06dba264ec67722420
Kushal Desai <kushaldesai1990@gmail.com>
2020-08-09 23:19:00 +0530

    Added attempt count, working code

--- 06/ScratchAndWin/lib/ScratchAndWin.dart
+++ 06/ScratchAndWin/lib/ScratchAndWin.dart
@@ -14,13 +14,17 @@ class _ScratchAndWinState extends State<ScratchAndWin> {
 
   // TODO: Get an array for 25 buttons
   List<String> itemArray;
+  String message;
   int luckyNumber;
+  int noOfAttempts;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     itemArray = List<String>.generate(25, (index) => "empty");
+    noOfAttempts = 5;
+    message = 'Only $noOfAttempts attempts are allowed';
     generateRandomNumber();
   }
 
@@ -32,15 +36,54 @@ class _ScratchAndWinState extends State<ScratchAndWin> {
   }
 
   // TODO: Define getImage method
+  AssetImage getImage(int index) {
+    String currentState = itemArray[index];
+    switch (currentState) {
+      case 'lucky':
+        return lucky;
+        break;
+      case 'unlucky':
+        return unlucky;
+        break;
+    }
+    return circle;
+  }
 
   // TODO: PlayGame methods
+  playGame(int index) {
+    if (luckyNumber == index) {
+      setState(() {
+        itemArray[index] = "lucky";
+        noOfAttempts = noOfAttempts - 1;
+        message = 'Only $noOfAttempts attempts are allowed';
+      });
+    } else {
+      setState(() {
+        itemArray[index] = "unlucky";
+        noOfAttempts = noOfAttempts - 1;
+        message = 'Only $noOfAttempts attempts are allowed';
+      });
+    }
+
+    if (noOfAttempts == 0) {
+      this.resetGame();
+    }
+  }
 
   // TODO: ShowAll
+  showAll() {
+    setState(() {
+      itemArray = List<String>.filled(25, "unlucky");
+      itemArray[luckyNumber] = "lucky";
+    });
+  }
 
   //TODO: ResetAll
   resetGame() {
     setState(() {
       itemArray = List<String>.filled(25, "empty");
+      noOfAttempts = 5;
+      message = 'Only $noOfAttempts attempts are allowed';
     });
     generateRandomNumber();
   }
@@ -50,6 +93,78 @@ class _ScratchAndWinState extends State<ScratchAndWin> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Scratch & Win'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+              child: GridView.builder(
+                  padding: EdgeInsets.all(20.0),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 5,
+                    childAspectRatio: 1.0,
+                    crossAxisSpacing: 10.0,
+                    mainAxisSpacing: 10.0,
+                  ),
+                  itemCount: itemArray.length,
+                  itemBuilder: (context, i) => SizedBox(
+                        width: 50.0,
+                        height: 50.0,
+                        child: RaisedButton(
+                          onPressed: () {
+                            playGame(i);
+                          },
+                          child: Image(image: this.getImage(i)),
+                        ),
+                      ))),
+          Container(
+              margin: EdgeInsets.all(15.0),
+              color: Colors.black,
+              child: Center(
+                child: Text(
+                  message,
+                  style: TextStyle(color: Colors.white, fontSize: 23.0),
+                ),
+              )),
+          Container(
+            margin: EdgeInsets.all(15.0),
+            child: RaisedButton(
+              onPressed: () {
+                this.showAll();
+              },
+              color: Colors.cyan,
+              padding: EdgeInsets.all(20.0),
+              child: Text('Show All'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(15.0),
+            child: RaisedButton(
+              onPressed: () {
+                this.resetGame();
+              },
+              color: Colors.cyan,
+              padding: EdgeInsets.all(20.0),
+              child: Center(
+                child: Text(
+                  'Reset Game',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.all(15.0),
+              color: Colors.black,
+              child: Center(
+                child: Text(
+                  'LearnCodeOnline.in',
+                  style: TextStyle(color: Colors.white, fontSize: 23.0),
+                ),
+              ))
+        ],
       ),
     );
   }
