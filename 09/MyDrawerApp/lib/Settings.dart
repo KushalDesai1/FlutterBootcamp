68c172d840b1815d82a764b2d98c4194d8fb8eb7
Kushal Desai <kushaldesai1990@gmail.com>
2020-08-26 15:52:23 +0530

    Added Drawer and user content

---
+++ 09/MyDrawerApp/lib/Settings.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('Setting here')],
+        ),
+      ),
+    );
+  }
+}
